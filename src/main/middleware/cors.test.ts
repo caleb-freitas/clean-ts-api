de96e8c075a8a512fd9b90a783c711839d26d067
caleb-freitas <calebfreitas@tutanota.com>
2022-02-15 20:01:30 -0300

    test: ensure cors is enabled

---
+++ src/main/middleware/cors.test.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import request from "supertest";
+
+import app from "../config/app";
+
+describe("CORS  Middleware", () => {
+  test("should enable CORS", async () => {
+    app.post("/test_cors", (request, response) => {
+      response.send();
+    });
+    await request(app)
+      .get("/test_cors")
+      .expect("access-control-allow-origin", "*")
+      .expect("access-control-allow-methods", "*")
+      .expect("access-control-allow-headers", "*");
+  });
+});
