b1b8f1b41bdfd7f6ec7bca3655f0603014914179
caleb-freitas <calebfreitas@tutanota.com>
2022-02-15 21:12:23 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,23 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from "supertest";
 
+import { MongoHelper } from "../../infra/db/mongodb/helper/mongodb-helper";
 import app from "../config/app";
 
 describe("SignUp Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
+  });
+
   test("should return account on succes", async () => {
     await request(app)
       .post("/api/signup")
