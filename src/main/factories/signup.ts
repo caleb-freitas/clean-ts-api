24a98c2fb180f66f4d6ca761dd3a85e1a037577e
caleb-freitas <calebfreitas@tutanota.com>
2022-02-18 20:52:04 -0300

    feat: add LogPrismaRepository to signup factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountPrismaRepository } from "../../infra/db/prisma/account-repository/account";
+import { LogPrismaRepository } from "../../infra/db/prisma/log-repository/log";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
 import { IController } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
@@ -9,9 +10,10 @@ import { LogControllerDecorator } from "../decorators/log";
 export const makeSignUpController = (): IController => {
   const salt = 12;
   const emailValidator = new EmailValidatorAdapter();
-  const accountPrismaRepository = new AccountPrismaRepository();
   const bcryptAdapter = new BcryptAdapter(salt);
+  const accountPrismaRepository = new AccountPrismaRepository();
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountPrismaRepository);
+  const logPrismaRepository = new LogPrismaRepository();
   const signUpController = new SignUpController(emailValidator, dbAddAccount);
-  return new LogControllerDecorator(signUpController);
+  return new LogControllerDecorator(signUpController, logPrismaRepository);
 };
