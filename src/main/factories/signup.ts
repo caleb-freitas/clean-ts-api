ff44b4cc3b4e7206f3de565ddefca29cb4dab8b6
caleb-freitas <calebfreitas@tutanota.com>
2022-02-18 15:51:31 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,13 +2,16 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountPrismaRepository } from "../../infra/db/prisma/account-repository/account";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { IController } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): IController => {
   const salt = 12;
   const emailValidator = new EmailValidatorAdapter();
   const accountPrismaRepository = new AccountPrismaRepository();
   const bcryptAdapter = new BcryptAdapter(salt);
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountPrismaRepository);
-  return new SignUpController(emailValidator, dbAddAccount);
+  const signUpController = new SignUpController(emailValidator, dbAddAccount);
+  return new LogControllerDecorator(signUpController);
 };
