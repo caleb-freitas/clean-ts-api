e5c90f88e1060b4bc56eae202a4fa58b1d7167bc
caleb-freitas <calebfreitas@tutanota.com>
2022-02-19 22:04:47 -0300

    test: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest } from "../../helpers/http-helper";
+import { badRequest, serverError } from "../../helpers/http-helper";
 import { IEmailValidator, IHttpRequest } from "../signup/signup-protocols";
 import { LoginController } from "./login";
 
@@ -68,5 +68,14 @@ describe("LoginController", () => {
     const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
     await sut.handle(makeFakeRequest());
     expect(isValidSpy).toHaveBeenCalledWith("any@mail.com");
+  });
+
+  test("should return 500 if EmailValidator theows", async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, "isValid").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
