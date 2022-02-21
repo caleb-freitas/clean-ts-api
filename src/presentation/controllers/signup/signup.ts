3d3c98e84caca3cd969a805db8b605acbd41777d
caleb-freitas <calebfreitas@tutanota.com>
2022-02-21 15:38:09 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,4 +1,4 @@
-import { InvalidParamError, MissingParamError } from "../../errors";
+import { InvalidParamError } from "../../errors";
 import { badRequest, ok, serverError } from "../../helpers/http-helper";
 import {
   IHttpResponse,
@@ -29,18 +29,6 @@ export class SignUpController implements IController {
       const error = this.validation.validate(httpRequest.body);
       if (error) {
         return badRequest(error);
-      }
-      const requiredParams = [
-        "name",
-        "email",
-        "password",
-        "passwordConfirmation",
-      ];
-      // eslint-disable-next-line no-restricted-syntax
-      for (const field of requiredParams) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
-        }
       }
       const { name, email, password, passwordConfirmation } = httpRequest.body;
       if (password !== passwordConfirmation) {
