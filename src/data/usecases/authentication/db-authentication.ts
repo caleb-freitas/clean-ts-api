33921ebe62514f8eb484756991f532014199a573
caleb-freitas <calebfreitas@tutanota.com>
2022-02-22 18:35:56 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -3,18 +3,22 @@ import {
   IAuthenticationModel,
 } from "../../../domain/usecases/authentication";
 import { IHashCompare } from "../../protocols/cryptography/hash-compare";
+import { ITokenGenerator } from "../../protocols/cryptography/token-generator";
 import { ILoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 
 export class DbAuthentication implements IAuthentication {
   private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository;
   private readonly hashCompare: IHashCompare;
+  private readonly tokenGenerator: ITokenGenerator;
 
   constructor(
     loadAccountByEmailRepository: ILoadAccountByEmailRepository,
-    hashCompare: IHashCompare
+    hashCompare: IHashCompare,
+    tokenGenerator: ITokenGenerator
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashCompare = hashCompare;
+    this.tokenGenerator = tokenGenerator;
   }
 
   async auth(authentication: IAuthenticationModel): Promise<string | null> {
@@ -23,6 +27,7 @@ export class DbAuthentication implements IAuthentication {
     );
     if (account) {
       await this.hashCompare.compare(authentication.password, account.password);
+      await this.tokenGenerator.generate(account.id);
     }
     return null;
   }
