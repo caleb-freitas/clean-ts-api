0397ee14e7704c11f433a53810e4a43e271c5954
caleb-freitas <calebfreitas@tutanota.com>
2022-02-21 21:27:08 -0300

    test: ensure RequiredFieldValidation not return if validation suceeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -4,9 +4,13 @@ import { RequiredFieldValidation } from "./required-field-validation";
 describe("RequiredFieldValidation", () => {
   test("should return a MissingParamError if validation fails", () => {
     const sut = new RequiredFieldValidation("field");
-    const error = sut.validate({
-      name: "any_name",
-    });
+    const error = sut.validate({ name: "any_name" });
     expect(error).toEqual(new MissingParamError("field"));
+  });
+
+  test("should not return if validation succeeds", () => {
+    const sut = new RequiredFieldValidation("field");
+    const error = sut.validate({ field: "any_name" });
+    expect(error).toBeFalsy();
   });
 });
