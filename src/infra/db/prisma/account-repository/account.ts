b889720e4c5cad09abfb08fb772be4a33b47a004
caleb-freitas <calebfreitas@tutanota.com>
2022-02-23 22:30:39 -0300

    feat: ensure AccountPrismaRepository updates the account access_token on updateAccessToken success

--- src/infra/db/prisma/account-repository/account.ts
+++ src/infra/db/prisma/account-repository/account.ts
@@ -1,12 +1,13 @@
 import { IAddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
 import { ILoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-by-email-repository";
+import { IUpdateAccessTokenRepository } from "../../../../data/protocols/db/update-access-token-repository";
 import { IAccountModel } from "../../../../domain/models/account";
 import { IAddAccountModel } from "../../../../domain/usecases/add-account";
 import { prisma } from "../client/prisma-client";
 
 export class AccountPrismaRepository
   // eslint-disable-next-line prettier/prettier
-  implements IAddAccountRepository, ILoadAccountByEmailRepository {
+  implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository {
   async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const { name, email, password } = accountData;
     const account = await prisma.accounts.create({
@@ -26,5 +27,16 @@ export class AccountPrismaRepository
       },
     });
     return account;
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    await prisma.accounts.update({
+      where: {
+        id,
+      },
+      data: {
+        access_token: token,
+      },
+    });
   }
 }
