7fe35b41e083f6896f2f4245aad04846b69df5ff
caleb-freitas <calebfreitas@tutanota.com>
2022-02-23 21:39:09 -0300

    refactor: move sut creation to an factory method

--- src/infra/criptography/jwp-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwp-adapter/jwt-adapter.spec.ts
@@ -8,9 +8,13 @@ jest.mock("jsonwebtoken", () => ({
   },
 }));
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter("secret");
+};
+
 describe("JwtAdapter", () => {
   test("should call sign with the correct values", async () => {
-    const sut = new JwtAdapter("secret");
+    const sut = makeSut();
     const signSpy = jest.spyOn(jwt, "sign");
     await sut.encrypt("any_id");
     expect(signSpy).toHaveBeenCalledWith(
@@ -22,13 +26,13 @@ describe("JwtAdapter", () => {
   });
 
   test("should return a token on sign success", async () => {
-    const sut = new JwtAdapter("secret");
+    const sut = makeSut();
     const accessToken = await sut.encrypt("any_id");
     expect(accessToken).toBe("token");
   });
 
   test("should throw if sign throws", async () => {
-    const sut = new JwtAdapter("secret");
+    const sut = makeSut();
     jest.spyOn(jwt, "sign").mockImplementationOnce(() => {
       throw new Error();
     });
