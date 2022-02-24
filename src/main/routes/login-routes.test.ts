3d0d2ef4ffaddb50ae64b90d1f72e9c26e428723
caleb-freitas <calebfreitas@tutanota.com>
2022-02-24 17:43:43 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,5 +1,7 @@
+import { hash } from "bcrypt";
 import request from "supertest";
 
+import { prisma } from "../../infra/db/prisma/client/prisma-client";
 import app from "../config/app";
 
 describe("Login Routes", () => {
@@ -12,6 +14,26 @@ describe("Login Routes", () => {
           email: "calebfreitas@tutanota.com",
           password: "123456",
           passwordConfirmation: "123456",
+        })
+        .expect(200);
+    });
+  });
+
+  describe("POST /api/login", () => {
+    test("should return 200 on login", async () => {
+      const password = await hash("123456", 12);
+      await prisma.accounts.create({
+        data: {
+          name: "caleb",
+          email: "calebfreitas@tutanota.com",
+          password,
+        },
+      });
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "calebfreitas@tutanota.com",
+          password: "123456",
         })
         .expect(200);
     });
